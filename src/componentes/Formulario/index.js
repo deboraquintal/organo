a04731617ff5de18d3c7af62817025558f8cc7cd
Debora Quintal <debora_quintal@hotmail.com>
2024-10-27 23:16:57 -0300

    primeiro commit

---
+++ src/componentes/Formulario/index.js
@@ -0,0 +1,61 @@
+import CampoTexto from "../CampoTexto";
+import ListaSuspensa from "../ListaSuspensa";
+import "./Formulario.css";
+import Botao from "../Botao";
+import { useState } from "react";
+
+const Formulario = (props) => {
+
+  const times = [
+    'Programação',
+    'Front-End',
+    'Data Science',
+    'Devops',
+    'UX e Design',
+    'Mobile',
+    'Inovação e Gestão'
+  ]
+
+  const [nome, setNome] = useState('')
+  const [cargo, setCargo] = useState('')
+  const [imagem, setImagem] = useState('')
+  const [time, setTime] = useState('')
+
+  const aoSalvar = (evento) => {
+    evento.preventDefault();
+    props.aoColaboradorCadastrado({nome, cargo, imagem, time});
+  }
+
+  return (
+    <section className="formulario">
+    <form onSubmit={aoSalvar}>
+      <h2>Preencha os dados para criar o card do colaborador</h2>
+      <CampoTexto 
+          obrigatorio={true} 
+          label="Nome" 
+          placeholder="Digite o seu nome" 
+          valor={nome}
+          aoAlterado={valor => setNome(valor)}/>
+      <CampoTexto 
+          label="Cargo" 
+          placeholder="Digite o seu cargo" 
+          valor={cargo}
+          aoAlterado={valor => setCargo(valor)}/>
+      <CampoTexto 
+          label="Imagem" 
+          placeholder="Digite o endereço da imagem" 
+          valor={imagem}
+          aoAlterado={valor => setImagem(valor)}/>
+      <ListaSuspensa 
+          obrigatorio={true} 
+          label="Time" 
+          itens={times} 
+          valor={time}
+          aoAlterado={valor => setTime(valor)}/>
+      <Botao>Criar Card</Botao>
+    </form>
+  </section>
+  )
+}
+
+export default Formulario;
---
+++ src/componentes/ListaSuspensa/index.js
@@ -0,0 +1,18 @@
+import './ListaSuspensa.css'
+
+const ListaSuspensa = (props) => {
+    console.log(props.itens)
+
+    return (
+        <div className='lista-suspensa'>
+            <label>{props.label}</label>
+            <select required={props.required} value={props.valor} onChange={evento => props.aoAlterado(evento.target.value)}>
+                {props.itens.map(item => {
+                    return <option key={item}>{item}</option>
+                })}
+            </select>
+        </div>
+    )
+}
+
+export default ListaSuspensa
